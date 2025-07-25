d856a9a3020d2c10a4bad10e3f505938cd2ff316
90135 <193311049+90135@users.noreply.github.com>
2025-07-25 17:12:28 +0800

    Try to fix VR Tablet Subtitle
    not tested

--- COM3D2.JustAnotherTranslator.Plugin/Subtitle/Component/VRTabletSubtitleComponent.cs
+++ COM3D2.JustAnotherTranslator.Plugin/Subtitle/Component/VRTabletSubtitleComponent.cs
@@ -9,97 +9,130 @@ namespace COM3D2.JustAnotherTranslator.Plugin.Subtitle.Component;
 /// </summary>
 public class VRTabletSubtitleComponent : BaseSubtitleComponent
 {
+    protected Camera _uiCamera;
+    protected int _uiLayer;
+
+    /// <summary>
+    ///     组件初始化
+    /// </summary>
+    protected void Awake()
+    {
+        FindUICameraAndLayer();
+    }
+
+    /// <summary>
+    ///     查找 UI 相机
+    /// </summary>
+    protected void FindUICameraAndLayer()
+    {
+        if (GameMain.Instance.OvrMgr != null && GameMain.Instance.OvrMgr.SystemUICamera != null)
+        {
+            UICamera vrUiCamera = GameMain.Instance.OvrMgr.SystemUICamera;
+            Camera uiCameraComponent = vrUiCamera.GetComponent<Camera>();
+            if (uiCameraComponent != null)
+            {
+                _uiCamera = uiCameraComponent;
+                _uiLayer = vrUiCamera.gameObject.layer;
+            }
+            else
+            {
+                LogManager.Warning("Cannot find UI Camera, VR tablet subtitle component will not be enabled/找不到 UI 相机，VR 平板字幕组件无法启用");
+            }
+        }
+    }
+
+
     /// <summary>
     ///     创建字幕UI
     /// </summary>
     protected override void CreateSubtitleUI()
     {
-        // 在 VR 模式中原有 UI 会被放到一个悬浮平板电脑上，见 OvrTablet
-        var canvasObj = new GameObject("JAT_Subtitle_SubtitleCanvas_VR_Tablet");
+        // 在 VR 模式中原有 UI 会被放到一个悬浮平板电脑上
+        // 通过创建一个 rtOVRUI Render Texture，然后把 UI Camera 重定向到这个 RT 上
+        // 然后把 rtOVRUI 设置为为 Odogu_TabletPC（平板电脑）的材质
+        // 因此我们需要使用相机渲染模式
 
-        // var ovrTablet = FindObjectOfType<OvrTablet>();
-        // if (ovrTablet is not null)
-        // {
-        //     var screenTransform = ovrTablet.transform.Find("Screen");
-        //     if (screenTransform is null)
-        //     {
-        //         LogManager.Warning(
-        //             "OvrTablet Screen not found, subtitles mar will not display on VR tablet/OvrTablet Screen 未找到，VR平板电脑上的字幕可能不会显示");
-        //         canvasObj.transform.SetParent(transform, false);
-        //     }
-        //     else
-        //     {
-        //         canvasObj.transform.SetParent(screenTransform, false);
-        //         canvasObj.layer = screenTransform.gameObject.layer;
-        //     }
-        // }
+        // 创建画布
+        var canvasObj = new GameObject("JAT_Subtitle_VRTabletSubtitleComponent_Canvas");
+        canvasObj.transform.SetParent(transform, false);
+        CanvasComponents = canvasObj.AddComponent<Canvas>();
+        CanvasComponents.renderMode = RenderMode.ScreenSpaceCamera; // 屏幕空间-相机
+        CanvasComponents.sortingOrder = 32767; // 确保在最上层显示
+        CanvasComponents.pixelPerfect = true;
+        CanvasComponents.overrideSorting = true;
 
-        var systemUI = GameObject.Find("SystemUI Root");
-        if (systemUI is not null)
+        if (_uiCamera != null)
         {
-            canvasObj.transform.SetParent(systemUI.transform, false);
-            canvasObj.layer = systemUI.layer;
+            CanvasComponents.worldCamera = _uiCamera;
+            canvasObj.layer = _uiLayer;
         }
         else
         {
-            canvasObj.transform.SetParent(transform, false);
-            LogManager.Warning(
-                "SystemUI Root not found, subtitles mar will not display on VR tablet/SystemUI Root 未找到，VR平板电脑上的字幕可能不会显示");
+            LogManager.Warning("Cannot find UI Camera, VR tablet subtitle component will not be enabled/找不到 UI 相机，VR 平板字幕组件无法启用");
+            return;
         }
 
-        CanvasComponents = canvasObj.AddComponent<Canvas>();
-        CanvasComponents.renderMode = RenderMode.WorldSpace;
-        CanvasComponents.sortingOrder = 32767; // 确保在最上层显示
-
-        // Canvas.transform.localPosition = Vector3.zero;
-        // Canvas.transform.localRotation = Quaternion.identity;
-        // Canvas.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
-
         // 添加画布缩放器
-        CanvasScalerComponents = CanvasComponents.gameObject.AddComponent<CanvasScaler>();
+        CanvasScalerComponents = canvasObj.AddComponent<CanvasScaler>();
         CanvasScalerComponents.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
         CanvasScalerComponents.referenceResolution = new Vector2(1920, 1080);
-        CanvasScalerComponents.matchWidthOrHeight = 0.5f;
+        CanvasScalerComponents.matchWidthOrHeight = 0.5f; // 0是宽度，1是高度，0.5是两者平衡
+        CanvasScalerComponents.dynamicPixelsPerUnit = 100; // 每个单位的像素数量
+
+        // 添加画布组
+        CanvasGroupComponents = CanvasComponents.gameObject.AddComponent<CanvasGroup>();
+        CanvasGroupComponents.alpha = 1f;
+        CanvasGroupComponents.blocksRaycasts = false; // 不阻挡射线（不阻止点击）
+        CanvasGroupComponents.interactable = false; // 不可交互
 
         // 创建背景面板
-        var backgroundObj = new GameObject("JAT_Subtitle_SubtitleBackground");
-        backgroundObj.transform.SetParent(CanvasComponents.transform, false);
+        var backgroundObj = new GameObject("JAT_Subtitle_VRTabletSubtitleComponent_Background");
+        backgroundObj.transform.SetParent(canvasObj.transform, false);
+        backgroundObj.layer = canvasObj.layer;
         BackgroundImageComponents = backgroundObj.AddComponent<Image>();
         BackgroundImageComponents.color = new Color(0, 0, 0, 0.5f); // 半透明黑色背景
+        BackgroundImageComponents.raycastTarget = false; // 不拦截射线
 
         // 设置背景位置和大小
         var backgroundRect = BackgroundImageComponents.rectTransform;
-        backgroundRect.anchorMin = new Vector2(0, Config.VerticalPosition);
-        backgroundRect.anchorMax = new Vector2(1, Config.VerticalPosition);
-        backgroundRect.pivot = new Vector2(0.5f, 0);
-        backgroundRect.sizeDelta = new Vector2(0, Config.SubtitleHeight);
-
+        backgroundRect.anchoredPosition3D =
+            new Vector3(0, 0, 0); // 位置，默认位置为 Canvas 左下角，X 为距离 Canvas 左边的距离，Y为距离 Canvas 下边的距离
+        var size = new Vector2(1920, 30);
+        backgroundRect.sizeDelta = size; // 大小，取决于 Canvas 的参考分辨率，这里设置为 1920 等于全屏宽
+        backgroundRect.anchorMin = new Vector2(0, 0);
+        backgroundRect.anchorMax = new Vector2(0, 0); // 锚点为Canvas左下角
+        backgroundRect.pivot = new Vector2(0, 0); // 轴心0，左下角
+        backgroundRect.rotation = Quaternion.identity; // 旋转0度
+        backgroundRect.localScale = new Vector3(1, 1, 1); // 缩放1倍
 
         // 创建文本对象
-        var textObj = new GameObject("JAT_Subtitle_SubtitleText");
-        textObj.transform.SetParent(backgroundRect, false);
+        var textObj = new GameObject("JAT_Subtitle_VRTabletSubtitleComponent_Text");
+        textObj.transform.SetParent(backgroundObj.transform, false);
+        textObj.layer = backgroundObj.layer;
         TextComponent = textObj.AddComponent<Text>();
+        TextComponent.raycastTarget = false; // 不拦截射线
+        TextComponent.supportRichText = true; // 支持富文本
 
-        // 设置文本位置和大小
+        // 设置文本位置和大小，与背景完全一致
         var textRect = TextComponent.rectTransform;
+        textRect.anchoredPosition3D = new Vector3(0, 0, 0); // 注意可以跟随父级移动，因此只需要移动背景即可
+        textRect.sizeDelta = size;
         textRect.anchorMin = new Vector2(0, 0);
-        textRect.anchorMax = new Vector2(1, 1);
-        textRect.pivot = new Vector2(0.5f, 0.5f);
-        textRect.sizeDelta = new Vector2(-40, -20); // 左右上下各留出10像素的边距
-        textRect.anchoredPosition = new Vector2(0, 0);
+        textRect.anchorMax = new Vector2(0, 0);
+        textRect.pivot = new Vector2(0, 0);
+        textRect.rotation = Quaternion.identity;
+        textRect.localScale = new Vector3(1, 1, 1);
 
         // 设置默认文本样式
-        TextComponent.alignment = TextAnchor.MiddleCenter;
-        TextComponent.horizontalOverflow = HorizontalWrapMode.Overflow;
-        TextComponent.verticalOverflow = VerticalWrapMode.Truncate;
-
-        // 设置文本和背景不拦截点击事件
-        TextComponent.raycastTarget = false;
-        BackgroundImageComponents.raycastTarget = false;
+        TextComponent.alignment = TextAnchor.MiddleCenter; // 居中对齐
+        TextComponent.horizontalOverflow = HorizontalWrapMode.Overflow; // 垂直溢出时自动换行
+        TextComponent.verticalOverflow = VerticalWrapMode.Truncate; // 水平溢出时截断
 
         // 添加描边组件
         OutlineComponents = TextComponent.gameObject.AddComponent<Outline>();
-        OutlineComponents.enabled = false;
+        OutlineComponents.effectColor = new Color(0, 0, 0, 0.5f); // 黑色描边颜色
+        OutlineComponents.effectDistance = new Vector2(1, 1); // 描边宽度
+
 
         LogManager.Debug("VRTabletSubtitleComponent Subtitle UI created");
     }

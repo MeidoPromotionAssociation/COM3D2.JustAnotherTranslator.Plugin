c848f1ef981f361cd2feaf80052ca3208b4566db
90135 <193311049+90135@users.noreply.github.com>
2025-09-04 15:10:27 +0800

    Change UI text translation hook entry point

--- COM3D2.JustAnotherTranslator.Plugin/Hooks/UI/UITextTranslatePatch.cs
+++ COM3D2.JustAnotherTranslator.Plugin/Hooks/UI/UITextTranslatePatch.cs
@@ -19,6 +19,7 @@ public static class UITextTranslatePatch
     ///     SceneDaily/ボタン画像/男エディット
     /// </summary>
     /// <param name="Term"></param>
+    /// <param name="Translation"></param>
     /// <param name="FixForRTL"></param>
     /// <param name="maxLineLengthForRTL"></param>
     /// <param name="ignoreRTLnumbers"></param>
@@ -27,38 +28,42 @@ public static class UITextTranslatePatch
     /// <param name="overrideLanguage"></param>
     /// <param name="__result"></param>
     /// <returns></returns>
-    [HarmonyPatch(typeof(LocalizationManager), "GetTranslation",
-        typeof(string),
-        typeof(bool),
-        typeof(int),
-        typeof(bool),
-        typeof(bool),
-        typeof(GameObject),
-        typeof(string)
-    )]
+    [HarmonyPatch(typeof(LocalizationManager), "TryGetTranslation")]
     [HarmonyPrefix]
-    public static bool LocalizationManager_GetTranslation_Prefix(string Term, bool FixForRTL,
+    public static bool LocalizationManager_TryGetTranslation_Prefix(
+        string Term,
+        ref string Translation,
+        bool FixForRTL,
         int maxLineLengthForRTL,
-        bool ignoreRTLnumbers, bool applyParameters, GameObject localParametersRoot,
+        bool ignoreRTLnumbers,
+        bool applyParameters,
+        GameObject localParametersRoot,
         string overrideLanguage,
-        ref string __result)
+        ref bool __result)
     {
         try
         {
-            LogManager.Debug($"LocalizationManager_GetTranslation_Prefix Term: {Term}");
+            LogManager.Debug($"LocalizationManager_TryGetTranslation_Prefix Term: {Term}");
 
             var result = UITranslateManager.HandleTextTermTranslation(Term);
 
             // 空内容则让原函数处理
             if (string.IsNullOrEmpty(result)) return true;
 
-            __result = result;
+            // 参数替换
+            if (applyParameters)
+            {
+                LocalizationManager.ApplyLocalizationParams(ref result, localParametersRoot);
+            }
+
+            Translation = result;
+            __result = true;
             return false;
         }
         catch (Exception e)
         {
             LogManager.Error(
-                $"LocalizationManager_GetTranslation_Prefix unknown error, please report this issue/未知错误，请报告此错误 {e.Message}\n{e.StackTrace}");
+                $"LocalizationManager_TryGetTranslation_Prefix unknown error, please report this issue/未知错误，请报告此错误 {e.Message}\n{e.StackTrace}");
         }
 
         return true;
